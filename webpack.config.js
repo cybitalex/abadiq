e0389a7441f366058765c873f08c2b2f49759f69
cybitalex <alex@cybitnetworks.com>
2023-08-16 23:32:34 -0400

    changed disabledHostCheck to allowedHosts

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
   target: 'web',
   devServer: {
     port: '5000',
-    allowedHosts: "all",,
+    allowedHosts: "all",
     static: {
       directory: path.join(__dirname, 'public')
 },
